4613dc4479763f9b7b46ee2f441a2938fcab661f
Katya Kulikova <katya.kulikova@jetbrains.com>
2017-04-10 12:38:07 +0300

    Initial commit

---
+++ Test131/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Test131
+{
+	public class Tests
+	{
+		[Fact]
+		public void Test1()
+		{
+			Assert.True (true);
+		}
+
+
+		[Fact]
+		public void Test2()
+		{
+			Assert.True (true);
+		}
+	}
+}
